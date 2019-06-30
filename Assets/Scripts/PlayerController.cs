3795d62dcf11ed57e1ce20131b58dce3a18ef380
timscholzInst <51216517+timscholzInst@users.noreply.github.com>
2019-06-29 20:47:15 -0400

    Player Movement Added
    Player Controller added to move the player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int moveSpeed;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Vector3 pos = transform.position;
+
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            pos.y += moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            pos.y -= moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            pos.x += moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            pos.x -= moveSpeed * Time.deltaTime;
+        }
+
+
+        transform.position = pos;
+    }
+
+}
